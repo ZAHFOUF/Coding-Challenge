27b9a83235a73e9863d588dab338f102d45d9c2f
ZAHFOUF YOUNES <youneszahfouf@gmail.com>
2023-07-05 13:46:59 +0100

    Task #2 : install mdb for the design

--- resources/views/pages/main.vue
+++ resources/views/pages/main.vue
@@ -1,9 +1,13 @@
 <script setup>
-
+import { MDBInput } from 'mdb-vue-ui-kit';
+import 'mdb-vue-ui-kit/css/mdb.min.css';
 
 </script>
 
 
 <template>
     <h2> Main App </h2>
+
+    <MDBInput label="Example label" v-model="input1" />
+
 </template>
